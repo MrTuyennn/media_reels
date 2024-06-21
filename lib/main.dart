6e34f2677c272657700729eb3c05db6c6b35db40
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-06-21 16:12:28 +0700

    init and setup app container

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:media_reels/src/app_container.dart';
+
+void main() {
+  runApp(const AppContainer());
+}
---
+++ lib/src/app_container.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppContainer extends StatefulWidget {
+  const AppContainer({super.key});
+
+  @override
+  State<AppContainer> createState() => _AppContainerState();
+}
+
+class _AppContainerState extends State<AppContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Jun--Cook"),
+    );
+  }
+}
