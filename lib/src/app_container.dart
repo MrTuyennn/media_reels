da18f6b5ee90070b09e6caa0f0a4d2c0c4879a37
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-07-10 10:49:50 +0700

    add freezed and bloc flutter --- test

--- lib/src/app_container.dart
+++ lib/src/app_container.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:media_reels/src/models/user/user.dart';
 
 class AppContainer extends StatefulWidget {
   const AppContainer({super.key});
@@ -8,10 +11,32 @@ class AppContainer extends StatefulWidget {
 }
 
 class _AppContainerState extends State<AppContainer> {
+  User? user;
+  User? user2;
+  @override
+  void initState() {
+    super.initState();
+    user = const User(firstName: "Nguyễn Ngọc Tuyên", lastName: "Phàm Phàm");
+    user2 = const User(firstName: "Nguyễn Ngọc Tuyên", lastName: "Phàm Phàm");
+
+    // final body = jsonEncode();
+    print(user!.toJson());
+    print(User.fromJson(user!.toJson()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Jun--Cook"),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.amber,
+            child: Text(user!.isActive ? "active" : "unactive"),
+          ),
+        ),
+      ),
     );
   }
 }
