4dbeebceda64b5616b57fa92d14b031b8ead1103
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-09-18 10:05:56 +0700

    add theme text flutter

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -13,26 +13,38 @@ ThemeData _buildLightTheme() {
     error: const Color(0xFFB00020),
   );
   return ThemeData(
-    useMaterial3: false,
-    brightness: Brightness.light,
-    colorScheme: colorScheme,
-    primaryColor: _primaryColor,
-    indicatorColor: Colors.white,
-    splashColor: Colors.white24,
-    splashFactory: InkRipple.splashFactory,
-    canvasColor: Colors.white,
-    scaffoldBackgroundColor: Colors.white,
-    buttonTheme: ButtonThemeData(
+      useMaterial3: false,
+      brightness: Brightness.light,
       colorScheme: colorScheme,
-      textTheme: ButtonTextTheme.primary,
-    ),
-    pageTransitionsTheme: PageTransitionsTheme(
-      builders: {
-        for (final p in TargetPlatform.values)
-          p: const CupertinoPageTransitionsBuilder()
-      },
-    ),
-  );
+      primaryColor: _primaryColor,
+      indicatorColor: Colors.white,
+      splashColor: Colors.white24,
+      splashFactory: InkRipple.splashFactory,
+      canvasColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
+      buttonTheme: ButtonThemeData(
+        colorScheme: colorScheme,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      pageTransitionsTheme: PageTransitionsTheme(
+        builders: {
+          for (final p in TargetPlatform.values)
+            p: const CupertinoPageTransitionsBuilder()
+        },
+      ),
+      textTheme: const TextTheme(
+          // display Small
+          displaySmall: TextStyle(fontSize: 11, fontWeight: FontWeight.w500),
+          // body Small
+          bodySmall: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+          //body medium
+          bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+          // body large
+          bodyLarge: TextStyle(fontSize: 19, fontWeight: FontWeight.w800),
+          // title medium
+          titleMedium: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          // title large
+          titleLarge: TextStyle(fontSize: 38, fontWeight: FontWeight.bold)));
 }
 
 ThemeData _buildDarkTheme() {
@@ -43,17 +55,29 @@ ThemeData _buildDarkTheme() {
     error: const Color(0xFFB00020),
   );
   return ThemeData(
-    useMaterial3: false,
-    brightness: Brightness.dark,
-    primaryColor: _primaryColor,
-    primaryColorDark: const Color(0xFF0050a0),
-    primaryColorLight: _secondaryColor,
-    indicatorColor: Colors.white,
-    canvasColor: const Color(0xFF202124),
-    scaffoldBackgroundColor: const Color(0xFF202124),
-    buttonTheme: ButtonThemeData(
-      colorScheme: colorScheme,
-      textTheme: ButtonTextTheme.primary,
-    ),
-  );
+      useMaterial3: false,
+      brightness: Brightness.dark,
+      primaryColor: _primaryColor,
+      primaryColorDark: const Color(0xFF0050a0),
+      primaryColorLight: _secondaryColor,
+      indicatorColor: Colors.white,
+      canvasColor: const Color(0xFF202124),
+      scaffoldBackgroundColor: const Color(0xFF202124),
+      buttonTheme: ButtonThemeData(
+        colorScheme: colorScheme,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      textTheme: const TextTheme(
+          // display Small
+          displaySmall: TextStyle(fontSize: 11, fontWeight: FontWeight.w500),
+          // body Small
+          bodySmall: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+          //body medium
+          bodyMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+          // body large
+          bodyLarge: TextStyle(fontSize: 19, fontWeight: FontWeight.w800),
+          // title medium
+          titleMedium: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          // title large
+          titleLarge: TextStyle(fontSize: 38, fontWeight: FontWeight.bold)));
 }
