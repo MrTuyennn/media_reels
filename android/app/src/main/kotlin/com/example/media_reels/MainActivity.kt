b910a111800904086e6bb4ef7653f19d3d6a86c1
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-09-29 12:46:59 +0700

    implement picker iamge android module

--- android/app/src/main/kotlin/com/example/media_reels/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/media_reels/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.media_reels
 
+import com.example.media_reels.ImagePicker.ImagePicker
 import com.example.media_reels.VideoPlayerPr.VideoPlayerFactory
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -7,6 +8,7 @@ import io.flutter.embedding.engine.FlutterEngine
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        ImagePicker(activity,flutterEngine.dartExecutor.binaryMessenger)
         flutterEngine
             .platformViewsController
             .registry
