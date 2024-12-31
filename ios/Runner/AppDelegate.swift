7421314949de4e2ba048a663643978c32a36f274
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2024-12-31 14:20:22 +0700

    Revert "pedding widget module ios"
    This reverts commit b4ae9efe0a21351653ec23a113926bd5644ccbd8.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,6 @@
 import Flutter
 import UIKit
 import PhotosUI
-import WidgetKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -22,53 +21,6 @@ import WidgetKit
         sound = Sound(messenger: controller.binaryMessenger)
         
         /***Module chanel***/
-        
-        /***Module widget***/
-        let storageChannel = FlutterMethodChannel(
-                name: "com.example.simpleIosWidgetExample/storage",
-                binaryMessenger: controller.binaryMessenger
-        )
-        storageChannel.setMethodCallHandler(
-                {
-                    (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-                    guard call.method == "savePreferences" else {
-                        result(
-                            FlutterMethodNotImplemented
-                        )
-                        return
-                    }
-                    
-                    guard let args = call.arguments as? [String: Any] else {
-                        result(
-                            FlutterError(
-                                code: "UNAVAILABLE",
-                                message: "It's required send arguments",
-                                details: nil
-                            )
-                        )
-                        return
-                    }
-                    
-                    guard let key = args["key"] as? String else {
-                        result(
-                            FlutterError(
-                                code: "UNAVAILABLE",
-                                message: "Its required send a key",
-                                details: nil
-                            )
-                        )
-                        return
-                    };
-                    
-                    StorageHelper.setValue(key: key, value: args["value"] as Any)
-                    
-                    WidgetCenter.shared.reloadTimelines(ofKind: "CounterWidget")
-                    
-                    result(StorageHelper.getString(key: key))
-                })
-        /***Module widget***/
-        
-        
         
         /*** view manager module*/
         
